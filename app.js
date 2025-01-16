1edcd1c0c813a9bebb10be44069de7932b951740
amrit-sa <amrit.rathore@dentsu.com>
2025-01-16 14:47:14 +0530

    Update function to accept delimeter if available and caluculate sum accordingly.

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const stringCalculator = (inputString) => {
-    let delimeter = ','
-    const numArray = inputString.split(delimeter).map(Number);
 
+    let delimeter = ','
+    if(inputString.startsWith('//') && isNaN(Number(inputString.charAt(2)))){
+        delimeter = inputString.charAt(2);
+    }
+    const numArray = inputString.split(delimeter).map(Number).filter(n => !isNaN(n))
+    
     const negativeNumbers = numArray.filter(n => n < 0);
     if(negativeNumbers.length) {
         throw new Error('Negative numbers not allowed: ' + negativeNumbers.join(', '));
